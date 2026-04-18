bb089e277cdf6970069506a1055eb11e8cd8d862
Oslo <99453047+jhnsaenz@users.noreply.github.com>
2026-04-18 15:01:35 -0500

    Revert "feat: create custom visual splash screen with logo"
    This reverts commit 7fdc71e6a422ea3750cacd125642e559a81af49e.

--- App.tsx
+++ App.tsx
@@ -1,17 +1,6 @@
 import "react-native-gesture-handler";
-import React, { useState, useEffect } from "react";
-import * as SplashScreen from "expo-splash-screen";
 import AppNavigator from "./src/navigation/AppNavigator";
-import Splash from "./src/screens/SplashScreen";
-
-SplashScreen.preventAutoHideAsync();
 
 export default function App() {
-  const [splashVisible, setSplashVisible] = useState(true);
-
-  return splashVisible ? (
-    <Splash onFinish={() => setSplashVisible(false)} />
-  ) : (
-    <AppNavigator />
-  );
+  return <AppNavigator />;
 }
